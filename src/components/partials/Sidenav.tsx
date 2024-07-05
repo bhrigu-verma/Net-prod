cfdf050e0c474d57bf82659c07b430de17a7e710
Bhrigu verma <bhriguverma.bt23cse@pec.edu.in>
2024-07-05 15:28:21 +0530

    responsiveness

--- src/components/partials/Sidenav.tsx
+++ src/components/partials/Sidenav.tsx
@@ -15,62 +15,64 @@ const Sidenav = () => {
             </h1>
             <nav className="flex flex-col text-zinc-400 text-xl gap-3">
                 <h1 className="text-white font-semibold text-2xl mt-10 mb-5">
-                    New Feeds
+                <div className="hidden md:block">  New feeds</div>
                 </h1>
                 <Link
-                    to="/trending"
-                    className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-5"
-                >
-                    <i className="ri-fire-fill"></i> Trending
-                </Link>
+            to="/trending"
+            className="flex items-center space-x-2 hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-2 md:p-5"
+        >
+            <i className="ri-fire-fill text-4xl sm:text-3xl md:text-2xl lg:text-xl"></i>
+            <div className="hidden  md:flex">Trending</div>
+        </Link>
                 <Link
                     to="/popular"
-                    className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-5"
+                    className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-0 md:p-5"
                 >
                     <i className="mr-2 ri-bard-fill"></i>
-                    Popular
+                    <div className="hidden md:flex"> Popular </div>
                 </Link>
                 <Link
                     to="/movie"
-                    className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-5"
+                    className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-0 md:p-5"
                 >
-                    <i className="mr-2 ri-movie-2-fill"></i>
-                    Movies
+                    <i className="mr-2 ri-movie-2-fill "></i>
+               <div className="hidden md:block">  </div>
+                    <div className="hidden md:block">Movies</div>
                 </Link>
                 <Link
                     to="/tv"
                     className="hover:bg-[#6556CD] hover:text-white duration-400 rounded-lg p-5"
                 >
                     <i className="mr-2 ri-tv-2-fill"></i>
-                    Tv Shows
+                    <div className="hidden md:block"> Tv shows </div>
                 </Link>
                 <Link
                     to="/person"
                     className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5 m-1"
                 >
                     <i className="mr-2 ri-team-fill"></i>
-                    People
+                    <div className="hidden md:block"> People </div>
                 </Link>
             </nav>
             <hr className="border-none h-[1px] bg-zinc-400" />
             <nav className="flex flex-col text-zinc-400 text-xl gap-3">
                 <h1 className="text-white font-semibold text-xl mt-10 mb-5">
-                    Website Information
+                <div className="hidden md:block">Website info  </div>
                 </h1>
                 <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
-                    <i className="mr-2 ri-information-fill"></i> About us
+                    <i className="mr-2 ri-information-fill"></i> <div className="hidden md:block"> About Us </div>
                 </Link>
                 <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5">
                     <i className="mr-2 ri-phone-fill"></i>
-                    Contact Us
+                    <div className="hidden md:block">Contact us  </div>
                 </Link>
                 <Link className="hover:bg-[#6556CD] hover:text-white duration-300 rounded-lg p-5"
                 to="https://bhriguverma-theta.vercel.app/"
                 >
                 <i className="ri-github-line"></i>
-                    B.Verma
+                <div className="hidden md:block">Bverma  </div>
                 </Link>
-                <div className="text-[12px] left-0 bottom-0"> copyright @VermaTechnologies</div>
+                <div className="text-[12px] left-0 bottom-0"> <div className="hidden md:block"> Copyright@bhriguverma </div></div>
             </nav>
         </div>
     );
